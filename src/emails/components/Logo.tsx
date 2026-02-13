3eb7f60381521f8055adf00da9fbd7caef489119
weddingringring <weddingringring@gmail.com>
2026-02-13 11:02:52 +0000

    Update Logo.tsx

--- src/emails/components/Logo.tsx
+++ src/emails/components/Logo.tsx
@@ -1,23 +1,14 @@
 import * as React from 'react'
+import { Img } from '@react-email/components'
 
 export const WeddingRingRingLogo = () => {
   return (
-    <svg width="240" height="60" viewBox="0 0 240 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      {/* Two overlapping rings */}
-      <g>
-        {/* Left ring */}
-        <circle cx="20" cy="30" r="12" stroke="#2D5016" strokeWidth="3" fill="none" />
-        <circle cx="20" cy="30" r="8" stroke="#87A878" strokeWidth="2" fill="none" />
-        
-        {/* Right ring (overlapping) */}
-        <circle cx="32" cy="30" r="12" stroke="#2D5016" strokeWidth="3" fill="none" />
-        <circle cx="32" cy="30" r="8" stroke="#87A878" strokeWidth="2" fill="none" />
-      </g>
-      
-      {/* Text: WeddingRingRing */}
-      <text x="52" y="38" fontFamily="'Crimson Text', Georgia, serif" fontSize="24" fontWeight="700" fill="#2D5016">
-        WeddingRingRing
-      </text>
-    </svg>
+    <Img
+      src={`${process.env.NEXT_PUBLIC_BASE_URL}/logo.png`}
+      alt="WeddingRingRing"
+      width="400"
+      height="100"
+      style={{ maxWidth: '200px', height: 'auto' }}
+    />
   )
 }
